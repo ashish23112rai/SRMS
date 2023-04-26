238e314d5bc96bd9ba6d80e01df9689abbb1b614
ashish23112rai <106386636+ashish23112rai@users.noreply.github.com>
2023-04-26 16:58:49 +0530

    Add files via upload

---
+++ SRMS/student_sidebar.php
@@ -0,0 +1,35 @@
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+    <header class="header">
+        <a href="studenthome.php"><img src="studentlogo.png" alt="Admin logo" class="studentlogo"></a>
+        <a href="studenthome.php" class="headline">Student Dashboard</a>
+
+        <div class="logout">
+            <a href="logout.php" class="btn btn-primary">Logout</a>
+        </div>
+    </header>
+
+    <aside>
+        <ul>
+            <li>
+                <a href="">My Profile</a>
+            </li>
+
+            <li>
+                <a href="">Results</a>
+            </li>
+
+            <li>
+                <a href="">My Performance</a>
+            </li>
+
+            <li>
+                <a href="">My Courses</a>
+            </li>
+
+        </ul>
+    </aside>
